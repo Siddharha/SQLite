8682fd89bdc4089af21b2237fcacc0a77a810c2c
Siddhartha Maji <siddhartha.ee.13@gmail.com>
2015-06-30 16:54:55 +0530

    List Adapter --Completed. :-)

--- app/src/main/java/com/siddhartha/com/sqlite/adapters/MyListAdapter.java
+++ app/src/main/java/com/siddhartha/com/sqlite/adapters/MyListAdapter.java
@@ -1,7 +1,71 @@
 package com.siddhartha.com.sqlite.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.siddhartha.com.sqlite.R;
+import com.siddhartha.com.sqlite.bean.items;
+
+import java.util.ArrayList;
+
 /**
- * Created by BLUEHORSE DEVLOPER on 6/30/2015.
+ * Created by Siddhartha Maji on 6/30/2015.
  */
-public class MyListAdapter {
+public class MyListAdapter extends BaseAdapter {
+
+    Context context;
+    ArrayList<items> arrayList;
+    LayoutInflater _layoutInflater;
+
+    public MyListAdapter(Context context, ArrayList<items> arrayList) {
+        this.context = context;
+        this.arrayList = arrayList;
+        _layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return arrayList.size();
+    }
+
+    @Override
+    public items getItem(int position) {
+        return arrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View rootView = convertView;
+        if(rootView == null)    //If it goes wrong....
+        {
+            rootView = _layoutInflater.inflate(R.layout.table_layout,parent,false);
+            ItemHolder itemHolder = new ItemHolder();
+            itemHolder.txt_name = (TextView)rootView.findViewById(R.id.Name_txt);
+            itemHolder.txt_number = (TextView)rootView.findViewById(R.id.txt_number);
+            itemHolder.txt_id = (TextView)rootView.findViewById(R.id.txt_id);
+
+            rootView.setTag(rootView);
+
+        }
+        ItemHolder newItemHolder = (ItemHolder)rootView.getTag();
+        newItemHolder.txt_name.setText(arrayList.get(position).getNames());
+        newItemHolder.txt_number.setText(arrayList.get(position).getNumbers());
+        newItemHolder.txt_id.setText(arrayList.get(position).getIndex());
+        return rootView;
+    }
+
+    private class ItemHolder
+    {
+       TextView txt_name,txt_number,txt_id;
+    }
 }
